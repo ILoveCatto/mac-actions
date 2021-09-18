7e1ffbd0706e8ea7c4fe7eb3d6630d31ce278f47
ILoveCatto <89790616+ILoveCatto@users.noreply.github.com>
2021-09-18 12:41:27 +0700

    Add docker script

--- script-mac.sh
+++ script-mac.sh
@@ -1,3 +1,9 @@
 # disable spotlight indexing
 sudo mdutil -i off -a
-echo Edit script-mac.sh in your fastmac repo to auto-run commands in your Mac instances
+
+#Docker
+mkdir -p ~/.docker/machine/cache
+curl -Lo ~/.docker/machine/cache/boot2docker.iso https://github.com/boot2docker/boot2docker/releases/download/v19.03.12/boot2docker.iso
+brew install docker docker-machine
+docker-machine create --driver virtualbox default
+docker-machine env default
