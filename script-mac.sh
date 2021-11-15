56a2144b66abcd66947276b71b4667ab2536f728
xcgc <89790616+xcgc@users.noreply.github.com>
2021-11-15 10:53:28 +0700

    Update script-mac.sh

--- script-mac.sh
+++ script-mac.sh
@@ -1,9 +1,4 @@
 # disable spotlight indexing
 sudo mdutil -i off -a
 
-#Docker
-mkdir -p ~/.docker/machine/cache
-curl -Lo ~/.docker/machine/cache/boot2docker.iso https://github.com/boot2docker/boot2docker/releases/download/v19.03.12/boot2docker.iso
-brew install docker docker-machine
-docker-machine create --driver virtualbox default
-docker-machine env default
+
