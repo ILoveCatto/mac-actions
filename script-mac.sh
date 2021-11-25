c24c55af846bf7fbe6f590e11d133f88c910293d
xcgc <89790616+xcgc@users.noreply.github.com>
2021-11-26 06:15:25 +0700

    Update script-mac.sh

--- script-mac.sh
+++ script-mac.sh
@@ -1,4 +1,13 @@
 # disable spotlight indexing
 sudo mdutil -i off -a
 
-
+# Create new user to get access to root
+sudo dscl . -create /Users/ci
+sudo dscl . -create /Users/ci UserShell /bin/bash
+sudo dscl . -create /Users/ci RealName "Crescent"
+sudo dscl . -create /Users/ci UniqueID 1001
+sudo dscl . -create /Users/ci PrimaryGroupID 80
+sudo dscl . -create /Users/ci NFSHomeDirectory /Users/vncuser
+sudo dscl . -passwd /Users/ci Binnic123!
+sudo dscl . -passwd /Users/ci Binnic123!
+sudo createhomedir -c -u ci > /dev/null
