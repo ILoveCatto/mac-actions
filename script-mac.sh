c2c5e5ecd92459b22959c3504a535bfb7c5e0944
xcgc <89790616+xcgc@users.noreply.github.com>
2021-12-19 10:21:31 +0700

    Add podman install script

--- script-mac.sh
+++ script-mac.sh
@@ -1,9 +1,14 @@
 # disable spotlight indexing
 sudo mdutil -i off -a
 
+# Install podman
+brew install podman
+podman machine init
+podman machine start
+
 # Create new user to get access to root
 sudo dscl . -create /Users/ci
-sudo dscl . -create /Users/ci UserShell /bin/bash
+sudo dscl . -create /Users/ci UserShell /bin/zsh
 sudo dscl . -create /Users/ci RealName "Crescent"
 sudo dscl . -create /Users/ci UniqueID 1001
 sudo dscl . -create /Users/ci PrimaryGroupID 80
